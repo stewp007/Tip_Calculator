f35240d933e801530199bad542075baee37941fb
Stewart Powell <35788705+stewp007@users.noreply.github.com>
2020-11-18 19:36:59 -0800

    Initial Commit

---
+++ Tip_Calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tip_Calculator
+//
+//  Created by Stewart Powell on 11/18/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
